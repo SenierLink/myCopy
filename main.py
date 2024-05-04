c12e3b6d9b8298f69b97780241187a89bb39e3f6
SenierLink <1325181337@qq.com>
2024-05-05 02:18:26 +0800

    适配macos系统的代码

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from pathlib import Path
 import shutil
 
-PATH = "D:\Document\工作\\0057"
-TARGE_PATH="D:\Document\工作\mp4"
+PATH = "./test"
+TARGE_PATH="./mymp4"
 
 def main():
     targe_path = Path(TARGE_PATH)
     if not targe_path.exists():
         targe_path.mkdir(parents=True)
     for item in Path(PATH).rglob("*"):
-        if "mp4" in item.suffix:
+        if "JPG" in item.suffix:
             old_name = item.name
             new_name = item.parent.name + old_name
-            new_file = TARGE_PATH+"\\"+new_name
+            new_file = TARGE_PATH+"/"+new_name
             shutil.copyfile(item.resolve(),new_file)
             print(item.resolve())
 
